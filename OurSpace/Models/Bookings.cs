722183a8b8bcd4e147e2b1cfdb15dfedd645e1b8
jarangali <jasperlindell.arangali.cics@ust.edu.ph>
2023-11-28 16:15:49 +0800

    Updated

--- OurSpace/Models/Bookings.cs
+++ OurSpace/Models/Bookings.cs
@@ -1,6 +1,14 @@
 ﻿namespace OurSpace.Models
 {
-    public class Email
+    public class Bookings
     {
+        public int AId { get; set; }
+        public string BName { get; set; }
+        public string BEmail { get; set; }
+        public DateOnly BDate { get; set; }
+        public TimeOnly BTime { get; set; }
+        public int BCNum { get; set; }
+        public int BPNum { get; set; }
+        public string BMessage { get; set; }
     }
 }
