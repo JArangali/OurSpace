1c5eb618cbbfcd3e24fa238bfc39db605143a8fb
jarangali <jasperlindell.arangali.cics@ust.edu.ph>
2023-11-28 22:00:29 +0800

    Again

--- OurSpace/Controllers/HomeController.cs
+++ OurSpace/Controllers/HomeController.cs
@@ -31,6 +31,11 @@ namespace OurSpace.Controllers
             return View();
         }
 
+        public IActionResult Bookings()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
