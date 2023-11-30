92225178c8ecabd343d6a9807767911e3d9d3e0f
jarangali <jasperlindell.arangali.cics@ust.edu.ph>
2023-11-30 11:00:25 +0800

    Hihi

--- OurSpace/Controllers/DashboardController.cs
+++ OurSpace/Controllers/DashboardController.cs
@@ -60,55 +60,55 @@ public class DashboardController : Controller
         return View("Index", _dbData.bookings);
     }
 
+    //[HttpGet]
+    //public IActionResult UpdateUser(int id)
+    //{
+    //    Bookings? bookings = _dbData.bookings.FirstOrDefault(st => st.BId == id);
+
+    //    if (bookings != null)//was an instructor found?
+    //        return View(bookings);
+
+    //    return NotFound();
+    //}
+
+    //[HttpPost]
+    //public IActionResult UpdateUser(Bookings bookingsChanges)
+    //{
+    //    Bookings? bookings = _dbData.bookings.FirstOrDefault(st => st.BId == bookingsChanges.BId);
+
+    //    if (bookings != null)
+    //    {
+    //        bookings.BName = bookingsChanges.BName;
+    //        bookings.BEmail = bookingsChanges.BEmail;
+    //        bookings.BCNum = bookingsChanges.BCNum;
+    //        bookings.BDate = bookingsChanges.BDate;
+    //        bookings.BTime = bookingsChanges.BTime;
+    //        bookings.BMessage = bookingsChanges.BMessage;
+    //        _dbData.SaveChanges();
+    //    }
+
+    //    return RedirectToAction("Index");
+    //}
+
     [HttpGet]
-    public IActionResult UpdateInstructor(int id)
+    public IActionResult creativesDone(int id)
     {
+
         Bookings? bookings = _dbData.bookings.FirstOrDefault(st => st.BId == id);
-
-        if (bookings != null)//was an instructor found?
-            return View(bookings);
-
-        return NotFound();
-    }
-
-    [HttpPost]
-    public IActionResult UpdateBookings(Bookings bookingsChanges)
-    {
-        Bookings? bookings = _dbData.bookings.FirstOrDefault(st => st.BId == bookingsChanges.BId);
-
         if (bookings != null)
         {
-            bookings.BName = bookingsChanges.BName;
-            bookings.BEmail = bookingsChanges.BEmail;
-            bookings.BCNum = bookingsChanges.BCNum;
-            bookings.BDate = bookingsChanges.BDate;
-            bookings.BTime = bookingsChanges.BTime;
-            bookings.BMessage = bookingsChanges.BMessage;
-            _dbData.SaveChanges();
-        }
-
-        return RedirectToAction("Index");
-    }
-
-    [HttpGet]
-    public IActionResult Done(int id)
-    {
-
-        Bookings? bookings = _dbData.bookings.FirstOrDefault(st => st.BId == id);
-
-        if (bookings != null)//was an instructor found?
             return View(bookings);
-
+        }
         return NotFound();
     }
 
     [HttpPost]
-    public IActionResult Done(Bookings newBookings)
+    public IActionResult creativesDone(Bookings newBookings)
     {
         Bookings? bookings = _dbData.bookings.FirstOrDefault(st => st.BId == newBookings.BId);
 
         if (bookings != null)
             _dbData.bookings.Remove(bookings);
-        return RedirectToAction("Index");
+        return View("Index", _dbData.bookings);
     }
 }
