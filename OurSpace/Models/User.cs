b455ed685378e1b51724991c5a76a22c92f177b5
lourdesashleyy <lourdesashley.santos.cics@ust.edu.ph>
2023-11-30 08:16:26 +0800

    Login SignUp

---
+++ OurSpace/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace OurSpace.Models;
+
+public class User
+{
+
+    public int UId { get; set; }
+    public string UFname { get; set; }
+    public string ULname { get; set; }
+    public DateTime UBirth { get; set; }
+    public string Uaddress { get; set; }
+    public int UNum { get; set; }
+    public string Uemail { get; set; }
+    public string UUsername { get; set; }
+    public string UPassword { get; set; }
+    public string UCPassword { get; set; }
+}
